21d2091dd1da0c0d73728317eac78abe61c1b6d1
Emad Abdelqader <emad.jehad@outlook.com>
2022-08-17 22:23:30 +0300

    Start building business tier

---
+++ ECS.Business/Repos/IUserRepo.cs
@@ -0,0 +1,24 @@
+﻿using ECS.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Business
+{
+    public interface IUserRepo
+    {
+        User? GetUser(int userId);
+
+        List<User> GetAll();
+
+        string GetUserName();
+
+        int Save(User user);
+
+        int Update(User user);
+
+        void Delete(int userId);
+    }
+}
---
+++ ECS.Business/Repos/UserRepo.cs
@@ -0,0 +1,73 @@
+﻿using ECS.DataAccess;
+using ECS.DataAccess.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Business
+{
+    public class UserRepo : IUserRepo
+    {
+        #region Private Members
+
+        private ApplicationContext _dc;
+
+        #endregion
+
+        #region Ctors..
+
+        public UserRepo()
+        {
+            _dc = new ApplicationContext();
+        }
+
+        #endregion
+
+        #region Get Methods
+
+        public List<User> GetAll()
+        {
+            // SELECT * FROM Users
+            return _dc.Users.ToList();
+        }
+
+        public User? GetUser(int userId) // 1, 2 , 5
+        {
+
+            return _dc.Users.Find(userId);
+        }
+
+        public string GetUserName()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Insert Methods
+
+        public int Save(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Delete Methods
+
+        public void Delete(int userId)
+        {
+
+        }
+
+        #endregion
+
+    }
+}
